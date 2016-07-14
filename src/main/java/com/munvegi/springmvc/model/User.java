d5784e1b4b2f98ceb561eadbd7c9f23b39ead5e1
munvegi <iban.munarriz@gmail.com>
2016-07-14 20:20:54 +0100

    Change toString method.

--- src/main/java/com/munvegi/springmvc/model/User.java
+++ src/main/java/com/munvegi/springmvc/model/User.java
@@ -87,8 +87,7 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", age=" + age
-				+ ", salary=" + salary + "]";
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + "]";
 	}
 
 
