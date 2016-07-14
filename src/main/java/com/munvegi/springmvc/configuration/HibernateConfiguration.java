0886a709f4906758fe850e6d84249d1ab1c57234
munvegi <iban.munarriz@gmail.com>
2016-07-14 20:45:30 +0100

    Rearrange method order.

--- src/main/java/com/munvegi/springmvc/configuration/HibernateConfiguration.java
+++ src/main/java/com/munvegi/springmvc/configuration/HibernateConfiguration.java
@@ -47,19 +47,19 @@ public class HibernateConfiguration {
         return dataSource;
     }
 
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
-        return properties;
-    }
-
     @Bean
     @Autowired
     public HibernateTransactionManager transactionManager(SessionFactory s) {
         HibernateTransactionManager txManager = new HibernateTransactionManager();
         txManager.setSessionFactory(s);
         return txManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        return properties;
     }
 }
