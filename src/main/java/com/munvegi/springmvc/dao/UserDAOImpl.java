208eda07790f6b06109e8251ed0c1bcae7545900
munvegi <iban.munarriz@gmail.com>
2016-07-14 18:24:47 +0100

    Remove unnecessary annotations.

--- src/main/java/com/munvegi/springmvc/dao/UserDAOImpl.java
+++ src/main/java/com/munvegi/springmvc/dao/UserDAOImpl.java
@@ -7,12 +7,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository("userDAO")
-@Transactional
+//@Transactional // Optional?
 public class UserDAOImpl implements UserDAO {
 
     @Autowired
