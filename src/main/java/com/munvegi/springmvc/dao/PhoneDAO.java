f4a31c982797473d42d6e04d3a4fa3f0a91da3b8
munvegi <iban.munarriz@gmail.com>
2016-11-03 22:31:42 +0000

    Insert new comment.

--- src/main/java/com/munvegi/springmvc/dao/PhoneDAO.java
+++ src/main/java/com/munvegi/springmvc/dao/PhoneDAO.java
@@ -3,7 +3,7 @@ package com.munvegi.springmvc.dao;
 import com.munvegi.springmvc.model.Phone;
 import com.munvegi.springmvc.model.User;
 
-// Develop branch changes
+// Develop branch changes again
 import java.util.List;
 
 public interface PhoneDAO {
